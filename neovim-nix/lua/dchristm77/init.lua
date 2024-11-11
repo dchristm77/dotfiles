b565695b8b09fb8a3413087e924713a8ee277c28
dchristm77 <derek.christman@gmail.com>
2024-11-10 18:03:18 -0800

    added catppuccin plugin

---
+++ neovim-nix/lua/dchristm77/init.lua
@@ -0,0 +1,28 @@
+require 'dchristm77.core.keymaps'
+require 'dchristm77.core.options'
+
+require('lze').register_handlers(require('lze.x'))
+
+require('lze').load {
+    { import = 'dchristm77.plugins.colortheme', },
+}
+
+
+  -- require 'plugins.colortheme'
+  -- require 'plugins.neotree'
+  -- require 'plugins.bufferline'
+  -- require 'plugins.lualine'
+  -- require 'plugins.treesitter'
+  -- require 'plugins.telescope'
+  -- require 'plugins.lsp'
+  -- require 'plugins.autocompletion'
+  -- require 'plugins.which-key'
+  -- require 'plugins.gitsigns'
+  -- require 'plugins.alpha'
+  -- require 'plugins.indent-blankline'
+  -- require 'plugins.misc'
+  -- require 'plugins.vim-tmux-navigator'
+  -- require 'plugins.conform'
+  -- require 'plugins.conjure'
+  -- require 'plugins.neogit'
+  -- require 'plugins.render-markdown'
